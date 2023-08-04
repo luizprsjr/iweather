6fca60232db9809ee48557beeec2415d48baaa59
Luiz Pires <lpiresjunior@gmail.com>
2023-08-04 02:41:15 -0300

    test: implemente tests for getCityByNameService

---
+++ src/services/getCityByNameService.test.ts
@@ -0,0 +1,13 @@
+import { mockCityAPIResponse } from '@__tests__/mocks/mockCityAPIResponse'
+
+import { api } from './api'
+import { getCityByNameService } from './getCityByNameService'
+
+describe('API: getCityByNameService', () => {
+  it('should return a list of cities', async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
+    const response = await getCityByNameService('São Paulo')
+
+    expect(response.length).toBeGreaterThan(0)
+  })
+})
