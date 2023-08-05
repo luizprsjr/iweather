d6a046af0c52dc3e020dad99f0fafe98a3687bee
Luiz Pires <lpiresjunior@gmail.com>
2023-08-05 03:41:58 -0300

    test: render dashboard screen when city is selected

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,6 +1,8 @@
+import { mockWeatherAPIResponse } from '@__tests__/mocks/api/mockWeatherAPIResponse'
 import { render } from '@__tests__/utils/customRender'
 import { saveStorageCity } from '@libs/asyncStorage/cityStorage'
-import { screen, waitFor } from '@testing-library/react-native'
+import { api } from '@services/api'
+import { act, screen, waitFor } from '@testing-library/react-native'
 
 import { Routes } from './'
 
@@ -8,6 +10,23 @@ describe('Routes', () => {
   it('should render the Search screen when no city is selected', async () => {
     render(<Routes />)
     const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+    expect(title).toBeTruthy()
+  })
+
+  it('should render dashboard screen when city is selected', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
+
+    const city = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+    await act(() => waitFor(() => render(<Routes />)))
+
+    const title = screen.getByText(city.name)
     expect(title).toBeTruthy()
   })
 })
